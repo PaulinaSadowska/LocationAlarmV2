48cce7846aded8e3a026bb7abca4dd5e638d40e1
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-05-07 19:10:13 +0200

    base of ChooseLocationActivity implemented with layout

--- app/src/main/java/com/nekodev/paulina/sadowska/locationalarmv2/ChooseLocationActivity.java
+++ app/src/main/java/com/nekodev/paulina/sadowska/locationalarmv2/ChooseLocationActivity.java
@@ -1,9 +1,13 @@
 package com.nekodev.paulina.sadowska.locationalarmv2;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.TextView;
 
+import com.appyvet.rangebar.IRangeBarFormatter;
+import com.appyvet.rangebar.RangeBar;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
@@ -13,64 +17,119 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class ChooseLocationActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final float STROKE_WIDTH = (float)1.5;
+    private static final int ZOOM = 13;
+    private static final int FILL_COLOR = Color.argb(60, 0, 0, 0);
+    private static final int STROKE_COLOR = Color.argb(200, 0, 0, 0);
     private GoogleMap mMap;
+    private PlaceAutocompleteFragment autocompleteFragment;
+
+    private LatLng placeLatLng;
+    private int radius = 1000;
+
+    @Bind(R.id.choose_location_radius_label)
+    TextView radiusLabel;
+    @Bind(R.id.choose_location_radius_seekbar)
+    RangeBar radiusSeekbar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_location);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        ButterKnife.bind(this);
         final SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.choose_location_map);
-        mapFragment.getMapAsync(this);
-
-        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+        autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.choose_location_place_autocomplete_fragment);
 
+        mapFragment.getMapAsync(this);
+        configureAutocompleteFragment();
+        radiusLabel.setText(getString(R.string.radius_label));
+        radiusSeekbar.setRangePinsByValue(0, radius);
+        radiusSeekbar.setOnRangeBarChangeListener(new RangeBar.OnRangeBarChangeListener() {
+            @Override
+            public void onRangeChangeListener(RangeBar rangeBar, int leftPinIndex,
+                                              int rightPinIndex,
+                                              String leftPinValue, String rightPinValue) {
+                radius = Integer.parseInt(rightPinValue);
+                updateMap(false);
+            }
+        });
+        radiusSeekbar.setFormatter(new IRangeBarFormatter() {
+            @Override
+            public String format(String s) {
+                // Transform the String s here then return s
+                return s + getString(R.string.radius_unit_label);
+            }
+        });
+    }
+
+    private void configureAutocompleteFragment() {
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             private final static String TAG = "PLACE_PICKER";
+
             @Override
             public void onPlaceSelected(Place place) {
-                mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(place.getLatLng()));
-                CameraPosition c = CameraPosition.builder()
-                        .target(place.getLatLng())
-                        .zoom(15)
-                        .build();
-                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(c), 3000, null);
+                placeLatLng = place.getLatLng();
+                updateMap(true);
             }
 
             @Override
             public void onError(Status status) {
-                // TODO: Handle the error.
-                Log.i(TAG, "An error occurred: " + status);
+                Log.e(TAG, "An error occurred: " + status);
             }
         });
-
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        placeLatLng = new LatLng(-34, 151);
+        updateMap(true);
+    }
+
+    private void updateMap(boolean animate){
+        if(mMap==null){
+            return;
+        }
+        mMap.clear();
+        mMap.addMarker(new MarkerOptions().position(placeLatLng));
+        if(radius>0)
+            addMarkerWithCircle();
+        if(animate){
+            moveCameraToPosition();
+        }
+    }
+
+    private void moveCameraToPosition() {
+        CameraPosition c = CameraPosition.builder()
+                .target(placeLatLng)
+                .zoom(ZOOM)
+                .build();
+        mMap.animateCamera(CameraUpdateFactory.newCameraPosition(c), 3000, null);
+    }
+
+    private void addMarkerWithCircle() {
+        if(radius>0){
+            mMap.addCircle(new CircleOptions()
+                    .center(placeLatLng)
+                    .radius(radius)
+                    .fillColor(FILL_COLOR)
+                    .strokeColor(STROKE_COLOR)
+                    .strokeWidth(STROKE_WIDTH));
+        }
     }
 }
