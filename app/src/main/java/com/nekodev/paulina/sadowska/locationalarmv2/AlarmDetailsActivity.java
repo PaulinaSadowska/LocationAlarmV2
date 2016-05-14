aa51ec87787976317a5a338cc215c884f07c931a
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-05-14 14:05:35 +0200

    new edit location icon, layout elements minded

--- app/src/main/java/com/nekodev/paulina/sadowska/locationalarmv2/AlarmDetailsActivity.java
+++ app/src/main/java/com/nekodev/paulina/sadowska/locationalarmv2/AlarmDetailsActivity.java
@@ -1,16 +1,38 @@
 package com.nekodev.paulina.sadowska.locationalarmv2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by Paulina Sadowska on 14.05.2016.
  */
 public class AlarmDetailsActivity extends AppCompatActivity {
 
+    @Bind(R.id.alarm_details_title)
+    TextView alarmTitle;
+    @Bind(R.id.alarm_details_map_preview)
+    ImageView mapPreview;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.alarm_details_activity);
+        ButterKnife.bind(this);
+
+        alarmTitle.setText("Piotrowo 2, Poznań");
+        mapPreview.setImageResource(R.drawable.maps_dummy);
+    }
+
+    @OnClick (R.id.alarm_details_edit_location_button)
+    public void onEditLocationButtonClick() {
+        Intent intent = new Intent(this, ChooseLocationActivity.class);
+        startActivity(intent);
     }
 }
