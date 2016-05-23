0a8c5e4a0e7cf9d362a3d5870fad3ce5e93a3778
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2016-05-23 21:58:26 +0200

    reseting mock data

--- app/src/main/java/com/nekodev/paulina/sadowska/locationalarmv2/data/DataManager.java
+++ app/src/main/java/com/nekodev/paulina/sadowska/locationalarmv2/data/DataManager.java
@@ -1,7 +1,9 @@
 package com.nekodev.paulina.sadowska.locationalarmv2.data;
 
+import com.google.android.gms.maps.model.LatLng;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.nekodev.paulina.sadowska.locationalarmv2.alarmDetails.AlarmTypes;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -22,6 +24,32 @@ public class DataManager {
         Type collectionType = new TypeToken<ArrayList<AlarmDataItem>>(){}.getType();
         String response = fileReader.readFromFile();
         mAlarmDataItems = gson.fromJson(response, collectionType);
+    }
+
+    public void resetMockAlarmData(){
+        AlarmDataItem item = new AlarmDataItem(1);
+        item.setIsActive(true);
+        item.setRadiusInMeters(3000);
+        item.setAlarmType(AlarmTypes.NOTIFICATION);
+        item.setLocation("London");
+         item.setRepeatDays(new boolean[]{true, false, false, true, false, true, false});
+        item.setCoordinates(new LatLng(1.33, 14.32));
+
+        AlarmDataItem item2 = new AlarmDataItem(1);
+        item2.setIsActive(false);
+        item2.setRadiusInMeters(500);
+        item2.setAlarmType(AlarmTypes.SOUND);
+        item2.setLocation("Paris");
+        item2.setRepeatDays(new boolean[]{true, true, true, true, true, true, true});
+        item2.setCoordinates(new LatLng(0.22, 22.3));
+
+        mAlarmDataItems.clear();
+        mAlarmDataItems.add(item);
+        mAlarmDataItems.add(item2);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(mAlarmDataItems);
+        fileReader.saveToFile(json);
     }
 
     public ArrayList<AlarmDataItem> getAlarmData(){
